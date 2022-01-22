b963843cedefc426a84d3924956e73d8a1317544
KJMAN678 <koji.smz@gmail.com>
2022-01-23 01:17:31 +0900

    車両の色分け実装。クリックした順番と出発点をテキスト表示

--- app.py
+++ app.py
@@ -1,85 +1,121 @@
-from bokeh.plotting import figure
-from bokeh.models import ColumnDataSource, Column
-from bokeh.io import curdoc
 from bokeh.events import Tap
-from bokeh.models import Button, CustomJS, Slider, Div, Arrow, VeeHead
-import optimize
+from bokeh.io import curdoc
+from bokeh.models import ColumnDataSource, Column, Button, CustomJS, Slider, Arrow, VeeHead
+from bokeh.layouts import row
+from bokeh.palettes import d3
+from bokeh.plotting import figure
 import numpy as np
+import optimize
 
-# ルート最適化用のインスタンス作成
-opt = optimize.root_opt()
+opt = optimize.root_opt() # ルート最適化用のインスタンス作成
+depo = (0, 0) # 出発点の座標
+coord_list=[depo] # クリックした場所の位置を保存するリスト。初期値として出発点の座標を設定。
 
-# クリックした場所の位置を保存するリスト
-coordList=[(0, 0)]
+bound = 10 # 表示するグラフの範囲
+plot = figure(
+            title='スライダーバーで指定した台数の車両でクリックした座標を巡回する',
+            tools="tap", 
+            height=600, 
+            width=1000,
+            x_range=(-bound*2, bound*2), 
+            y_range=(-bound, bound),
+          )
 
-TOOLS = "tap"
-bound = 10
-p = figure(title='スライダーバーで指定した台数の車両でクリックした座標を巡回する',
-           tools=TOOLS, height=500, width=700,
-           x_range=(-bound, bound), y_range=(-bound, bound),
-           sizing_mode="stretch_width"
-           )
+source = ColumnDataSource(data=dict(x=[], y=[]))           # 全体で使用する bokeh 用データ
+plot.circle(source=source,x='x',y='y', radius=0.2, alpha=0.8) # クリックして作成する巡回先を点として表示
+plot.diamond([0], [0], size=30, color="red", alpha=0.8)       # 出発点をひし形として表示
+plot.text(
+          x=[0.5], 
+          y=[0.5], 
+          text=["出発点"], 
+          text_font_size="18px",
+          text_baseline="middle", 
+          text_align="center"
+        )
 
-source = ColumnDataSource(data=dict(x=[], y=[]))   
-p.circle(source=source,x='x',y='y', radius=0.2, alpha=0.8)
-p.diamond([0], [0], size=30, color="red", alpha=0.8)
-
-# クリックすると 点を追加する
+click_count = 1
+# クリックすると点を追加する処理の関数
 def callback_click(event):
-    global coordList
-    Coords=(event.x,event.y)
-    coordList.append(Coords) 
-    source.data = dict(x=[i[0] for i in coordList], y=[i[1] for i in coordList])
+    
+    global ds
+    global coord_list
+    global click_count
+    
+    coord=(event.x,event.y)  # クリックした位置の座標をタプルに保存
+    coord_list.append(coord) # 座標をリストに保存
+    source.data = dict(      # 他の機能で共通して使えるよう座標を souse に保存
+              x=[i[0] for i in coord_list], 
+              y=[i[1] for i in coord_list]
+            )
+    plot.text( # クリックした回数をテキスト表示
+          x=[event.x+0.5], 
+          y=[event.y+0.5], 
+          text=[click_count], 
+          text_font_size="18px",
+          text_baseline="middle", 
+          text_align="center"
+        )
+    click_count += 1         # クリックした回数をカウント
 
-p.on_event(Tap, callback_click)
+# クリックイベントを反映
+plot.on_event(Tap, callback_click)
 
-result = ""
-### ボタン処理
+### ボタン処理の関数
 def callback_button():
-    global result
-    all_route_result_coodList, route_result_list, distance = opt.route_optimize_result(coordList, slider.value)
+  
+    # 最適化関数を実行して最適ルートの座標を取得
+    all_route_result_coodList, route_result_list, distance = opt.route_optimize_result(
+                                                              coord_list,  # クリックして決定した座標のリスト
+                                                              slider.value # スライダーバーで決定した車両台数
+                                                              )
         
+    # ルート順及びルートの向きをグラフに表示する処理
     for i in range(len(all_route_result_coodList)):
-      if len(all_route_result_coodList[i]) == 1:
+      
+      # 出発点の座標以外を巡回しない場合は処理しない
+      if len(all_route_result_coodList[i]) <= 2:
         continue
       
-      p.line(
+      # 各ルートの軌跡をラインで表示。次の矢印でも軌跡は表示できるが、凡例を作成するために使用。
+      plot.line(
         np.array(all_route_result_coodList[i])[:, 0],
         np.array(all_route_result_coodList[i])[:, 1],
-        legend_label=str(i)
-        
+        legend_label=str(i),
+        color=d3['Category10'][10][i]
       )
       
+      # 各ルートの方向を矢印として表示
       for j in range(len(all_route_result_coodList[i])-1):
         
-        p.add_layout(
+        plot.add_layout(
                 Arrow(
-                    end=VeeHead(line_color="firebrick", line_width=0.1),
-                    x_start=all_route_result_coodList[i][j][0],
-                    y_start=all_route_result_coodList[i][j][1],
-                    x_end=all_route_result_coodList[i][j+1][0],
-                    y_end=all_route_result_coodList[i][j+1][1],
+                      end=VeeHead(line_color="firebrick", line_width=0.1),
+                      x_start=all_route_result_coodList[i][j][0],
+                      y_start=all_route_result_coodList[i][j][1],
+                      x_end=all_route_result_coodList[i][j+1][0],
+                      y_end=all_route_result_coodList[i][j+1][1],
+                      line_color=d3['Category10'][10][i],
                     )
         )
         
-        p.add_layout(p.legend[0], 'right')
+        plot.add_layout(plot.legend[0], 'right') # 凡例の追加
         
-    
-
-
-# add a button widget andconfigure with the call back
-button = Button(label="Press Me")
+# ボタンウィジェットの実装
+button = Button(label="最適化の実行")
 button.on_click(callback_button)
 
-## スライダー
+## 車両台数を決めるスライダー
 slider = Slider(start=1, end=10, value=3, step=1, title="車両台数")
 slider.js_on_change("value", CustomJS(code="""
     console.log('slider: value=' + this.value, this.toString())
 """))
 
-print(slider.value)
+# 各機能をWeb画面にレイアウトする
+# layout=Column(row(slider, button), plot, sizing_mode='scale_width')
 
-layout=Column(p, slider, button)
+widgets = Column(slider, button, sizing_mode="fixed", height=250, width=150)
 
-# ブラウザに表示するために必要
+layout = Column(row(plot, widgets), sizing_mode='scale_width')
+
+# ブラウザに表示するために必要な処理
 curdoc().add_root(layout)
--- optimize.py
+++ optimize.py
@@ -1,122 +1,121 @@
+from scipy.spatial import distance_matrix
 from ortools.constraint_solver import routing_enums_pb2
 from ortools.constraint_solver import pywrapcp
 
-from scipy.spatial import distance_matrix
-
-solution_limit_times = 1000     # 計算回数制限
-solution_limit_seconds = 30 # 計算秒数制限
+# パラメーター
+solution_limit_times = 1000  # 最適化の計算回数制限
+solution_limit_seconds = 30  # 最適化の計算秒数制限
 
 class root_opt:
-
-    def create_data_model(self, coodList, vehicle_num):
+  
+    # 最適化用のデータセット作成
+    def create_data_model(self, cood_list, vehicle_num):
 
         data = {}
-        data['locations'] = coodList
-        data['distance_matrix'] = distance_matrix(
+        data['locations'] = cood_list              # 座標
+        data['distance_matrix'] = distance_matrix( # 距離
                             data['locations'], 
                             data['locations']
                             ).tolist()
-        data['num_vehicles'] = vehicle_num
-        data['depot'] = 0
+        data['num_vehicles'] = vehicle_num         # 車両台数
+        data['depot'] = 0                          # 出発点のインデックス
         return data
 
+    # ルート順のインデックスを作成する
     def output_route_list(self, data, manager, routing, solution):
 
-        all_route_list = []
-
-        max_route_distance = 0
+        all_route_list = []    # 全車両のルート順を保存するリスト
+        max_route_distance = 0 # 距離を保存する変数
 
         for vehicle_id in range(data['num_vehicles']):
 
-            each_route_list = []
+            each_route_list = [] # 1つの車両のルート順を保存するリスト
 
             index = routing.Start(vehicle_id)
-            plan_output = 'Route for vehicle {}:\n'.format(vehicle_id)
             route_distance = 0
             while not routing.IsEnd(index):
 
-                each_route_list.append(manager.IndexToNode(index))
-
-                # plan_output += ' {} -> '.format(manager.IndexToNode(index))
+                each_route_list.append(manager.IndexToNode(index)) # 巡回する場所のインデックスをリストに追加
                 previous_index = index
                 index = solution.Value(routing.NextVar(index))
                 route_distance += routing.GetArcCostForVehicle(
                     previous_index, index, vehicle_id)
-            # plan_output += '{}\n'.format(manager.IndexToNode(index))
+                
             each_route_list.append(manager.IndexToNode(index))
-            # plan_output += 'Distance of the route: {}m\n'.format(route_distance)
             max_route_distance = max(route_distance, max_route_distance)
-            all_route_list.append(each_route_list)
+            all_route_list.append(each_route_list) # 1つの車両のルート順を全体のルート順のリストに保存
 
         return all_route_list, max_route_distance
 
-    def route_optimize_result(self, coodList, vehicle_num):
+    # ルート最適化の実行
+    def route_optimize_result(self, cood_list, vehicle_num):
 
-        # Instantiate the data problem.
-        data = self.create_data_model(coodList, vehicle_num)
+        # データセットの作成
+        data = self.create_data_model(cood_list, vehicle_num)
 
-        # Create the routing index manager.
-        manager = pywrapcp.RoutingIndexManager(len(data['distance_matrix']),
-                                            data['num_vehicles'], data['depot'])
+        # ルーティングインデックスマネージャーの作成
+        manager = pywrapcp.RoutingIndexManager(
+                                len(data['distance_matrix']),
+                                data['num_vehicles'], data['depot']
+                              )
 
-        # Create Routing Model.
+        # ルーティングモデルの作成
         routing = pywrapcp.RoutingModel(manager)
 
-        # Create and register a transit callback.
+        # 距離の制約用の関数
         def distance_callback(from_index, to_index):
-            """Returns the distance between the two nodes."""
-            # Convert from routing variable Index to distance matrix NodeIndex.
             from_node = manager.IndexToNode(from_index)
             to_node = manager.IndexToNode(to_index)
             return data['distance_matrix'][from_node][to_node]
 
         transit_callback_index = routing.RegisterTransitCallback(distance_callback)
 
-        # Define cost of each arc.
+        # コストの定義
         routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
 
-        # Add Distance constraint.
+        # 距離の制約を設定
         dimension_name = 'Distance'
         routing.AddDimension(
             transit_callback_index,
             0,  # no slack
-            3000,  # vehicle maximum travel distance
+            3000,  # 最大距離
             True,  # start cumul to zero
-            dimension_name)
+            dimension_name
+            )
         distance_dimension = routing.GetDimensionOrDie(dimension_name)
         distance_dimension.SetGlobalSpanCostCoefficient(100)
 
-        # Setting first solution heuristic.
+        # ヒューリスティック解を求めるよう設定
         search_parameters = pywrapcp.DefaultRoutingSearchParameters()
         search_parameters.first_solution_strategy = (
             routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
         
-        # 計算制限
-        search_parameters.solution_limit = solution_limit_times       # 計算回数
-        search_parameters.time_limit.seconds = solution_limit_seconds # 計算秒数
+        # 計算制限を設定
+        search_parameters.solution_limit = solution_limit_times       # 計算の回数
+        search_parameters.time_limit.seconds = solution_limit_seconds # 計算の秒数
 
-        # Solve the problem.
+        # 最適化の実行
         solution = routing.SolveWithParameters(search_parameters)
 
-        # Print solution on console.
+        # 最適化の解を取得
         if solution:
+          
             route_result_list, distance = self.output_route_list(data, manager, routing, solution)
             
             # ルート順からルート順の座標リストを作成
-            all_route_result_coodList = []
+            all_route_result_cood_list = []
 
             for i in range(len(route_result_list)):
-                each_route_result_coodList = []
+                each_route_result_cood_list = []
 
                 for j in range(len(route_result_list[i])):
-                  each_route_result_coodList.append(coodList[route_result_list[i][j]])
+                  each_route_result_cood_list.append(cood_list[route_result_list[i][j]])
                 
-                all_route_result_coodList.append(each_route_result_coodList)
-            
+                all_route_result_cood_list.append(each_route_result_cood_list)
             
         else:
             distance = 0
             route_result_list = []
-            all_route_result_coodList = []
+            all_route_result_cood_list = []
 
-        return all_route_result_coodList, route_result_list, distance
+        return all_route_result_cood_list, route_result_list, distance
